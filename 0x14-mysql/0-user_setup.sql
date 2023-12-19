b742fb96f341f53873a58b8c37a479da2aab3d35
root <esllam.saeid@gmail.com>
2023-12-19 09:36:40 -0800

    delete

--- 0x14-mysql/0-user_setup.sql
+++
@@ -1,8 +0,0 @@
--- script to setup replica user on master sql server
-
-CREATE USER IF NOT EXISTS 'holberton_user'@'%' IDENTIFIED BY 'password';
-GRANT REPLICATION SLAVE ON *.* TO 'holberton_user'@'%';
-GRANT SELECT ON *.* TO 'holberton_user'@'%';
-
--- check that it was configured properly 
-SELECT user, Repl_slave_priv FROM mysql.user;
